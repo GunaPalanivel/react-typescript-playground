10bb543242d89df43c23b46d285cba330a56c916
GunaPalanivel <gp5901@srmist.edu.in>
2024-08-04 01:00:49 +0530

    chore: Update Bootstrap to version 5.2.3

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+// PascaleCasing: PascalCasing
+function Message() {
+  //JSX: JavaScript XML
+  const name = "Guna Palanivel";
+  if (name) return <h1>Hi There 👋, I'm {name}</h1>;
+  return <h1>Hi There 👋, I'm Guest</h1>;
+}
+
+export default Message;
