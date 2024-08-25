c073f344dc804793824822bc0a6ba07397a93099
GunaPalanivel <gp5901@srmist.edu.in>
2024-08-25 20:44:40 +0530

    refactor: Update ListGroup component to use dynamic rendering of items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,44 @@
+// function ListGroup() {
+//   return (
+//     <ul className="list-group">
+//       <li className="list-group-item">An item</li>
+//       <li className="list-group-item">A second item</li>
+//       <li className="list-group-item">A third item</li>
+//       <li className="list-group-item">A fourth item</li>
+//       <li className="list-group-item">And a fifth one</li>
+//     </ul>
+//   );
+// }
+
+// export default ListGroup;
+
+import React, { useState } from "react";
+import "../ListGroup.css";
+
 function ListGroup() {
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const items = [
+    "An item",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
+  ];
+
   return (
     <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">Abi Gowsalya</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
+      {items.map((item, index) => (
+        <li
+          key={index}
+          className={`list-group-item ${
+            index === selectedIndex ? "active" : ""
+          }`}
+          onClick={() => setSelectedIndex(index)}
+        >
+          {item}
+        </li>
+      ))}
     </ul>
   );
 }
